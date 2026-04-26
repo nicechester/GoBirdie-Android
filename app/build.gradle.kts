741de5a4451ba2946097e5bcfb2138b12ff5778e
Chester Kim <violkim@gmail.com>
2026-04-25 22:48:28 -0700

    chore: bump version to 1.1.1 (10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.nicechester.gobirdie"
         minSdk = 27
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.1.0"
+        versionCode = 10
+        versionName = "1.1.1"
         testInstrumentationRunner = "io.github.nicechester.gobirdie.HiltTestRunner"
     }
 
